ded40f6acd75114f1f1144843835574658d0052d
Jasmine Gutierrez <Jasmineg01@comcast.net>
2025-07-28 13:38:57 -0500

    function created

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 let inputs = prompt("Enter a list of comma-separated froyo flavors: ");
 let flavors = inputs.split(',');
 
-let flavorCounts = {};
+function countFrequencies(flavorsArray) {
+    let flavorCounts = {};
 
-for (let flavor of flavorsArray)
-    if (flavorCounts[flavor]) {
-        flavorCounts[flavor]++;
-    } else {
-        flavorCounts[flavor] = 1;
+    for (let flavor of flavorsArray) {
+        if (flavorCounts[flavor]) {
+            flavorCounts[flavor]++;
+        } else {
+            flavorCounts[flavor] = 1;
+        }
     }
     return flavorCounts;
+}
+
+let flavorOrderCounts = countFrequencies(flavors);
+
+console.log("Here's how many of each froyo flavor you ordered:", flavorOrderCounts);
