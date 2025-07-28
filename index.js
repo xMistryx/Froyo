de5facded693c1ce53aa06f54a9e44ee244ae575
Jasmine Gutierrez <Jasmineg01@comcast.net>
2025-07-28 13:34:12 -0500

    for loop created

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 let inputs = prompt("Enter a list of comma-separated froyo flavors: ");
 let flavors = inputs.split(',');
 
+let flavorCounts = {};
+
+for (let flavor of flavorsArray)
+    if (flavorCounts[flavor]) {
+        flavorCounts[flavor]++;
+    } else {
+        flavorCounts[flavor] = 1;
+    }
